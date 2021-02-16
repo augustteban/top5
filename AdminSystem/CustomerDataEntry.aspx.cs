d96f4216dbc80bad7e6dac2137387257b6572f00
augustteban <P2541807@my365.dmu.ac.uk>
2021-02-16 16:06:16 +0000

    Create the Find methof in the Presentation Layer for Customer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -30,4 +30,37 @@ public partial class _1_DataEntry : System.Web.UI.Page
         //navigate to the viewer page
         Response.Redirect("CustomerViewer.aspx");
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsCustomer Customer = new clsCustomer();
+        Int32 CustomerId;
+        bool isNumeric = int.TryParse(txtCustomerId.Text, out CustomerId);
+
+        if (!isNumeric)
+        {
+            lblError.Text = "Provided ID is not a number.";
+        }
+        else
+        {
+            Boolean Found = false;
+            Found = Customer.Find(CustomerId);
+
+            if (Found == true)
+            {
+                txtName.Text = Customer.Name;
+                txtEmailAddress.Text = Customer.EmailAddress;
+                txtAddress.Text = Customer.Address;
+                txtPassword.Text = Customer.Password;
+                txtLoyaltyPoints.Text = Customer.LoyaltyPoints.ToString();
+                txtCreatedAt.Text = Customer.CreatedAt.ToString();
+                chkIsEmailConfirmed.Checked = Customer.isEmailConfirmed;
+                lblError.Text = "";
+            }
+            else
+            {
+                lblError.Text = "The customer with this ID has not been found.";
+            }
+        }
+    }
 }

ab9b247151f63637be0d82fb040d0eebc1a32d2d
Rami Althubaiti <P2547643@my365.dmu.ac.uk>
2021-02-16 02:26:52 +0000

    Added testing and class for Order management

---
+++ Testing5/clsOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Testing5
+{
+    public class clsOrder
+    {
+        internal string Description;
+
+        public bool Active { get; internal set; }
+        public int OrderId { get; internal set; }
+        public int Price { get; internal set; }
+        public DateTime Order_date { get; internal set; }
+        public int ItemId { get; internal set; }
+        public int Size { get; internal set; }
+        public int Quantity { get; internal set; }
+    }
+}

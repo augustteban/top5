f75473e8fa0bcef33d370e6d96cc54a5414d73f1
Dkada <P2424196@my365.dmu.ac.uk>
2021-02-17 14:18:24 +0000

    added properties

--- ClassLibrary/clsStock2.cs
+++ ClassLibrary/clsStock2.cs
@@ -2,5 +2,7 @@
 {
     public class clsStock2
     {
+        public int Stock2 { get; set; }
+        public string ProductName { get; set; }
     }
 }
--- Testing4/tstStock2.cs
+++ Testing4/tstStock2.cs
@@ -13,5 +13,24 @@ namespace Testing4
             clsStock2 AnStock2 = new clsStock2();
             Assert.IsNotNull(AnStock2);
         }
+
+        [TestMethod]
+        public void ProductidPropertOK()
+        {
+            clsStock2 AnStock2 = new clsStock2();
+            int TestData = 1;
+            AnStock2.Stock2 = TestData;
+            Assert.AreEqual(AnStock2.Stock2, TestData);
+        }
+           
+        [TestMethod]   
+        public void ProductNameOK()
+        {
+            clsStock2 Anstock2 = new clsStock2();
+            string TestData = "Top";
+            Anstock2.ProductName = TestData;
+            Assert.AreEqual(Anstock2.ProductName, TestData);
+
+        }
     }
 }

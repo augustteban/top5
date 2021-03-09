10d3b2e002b09102668a624683717fa6845761cf
augustteban <P2541807@my365.dmu.ac.uk>
2021-03-09 17:19:39 +0000

    Create the Collection Class Properties for middle layer

---
+++ Testing2/tstCustomerCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace TestingCustomer
+
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            Assert.IsNotNull(AllCustomers);
+        }
+
+        [TestMethod]
+        public void CustomerListOK()
+        {
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            List<clsCustomer> TestList = new List<clsCustomer>();
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.Name = "John Smith";
+            TestItem.EmailAddress = "jsmith1987@gmail.com";
+            TestItem.Address = "Flat 10, Charles street, Leicester";
+            TestItem.Password = "secret123";
+            TestItem.isEmailConfirmed = true;
+            TestItem.LoyaltyPoints = 10.5;
+            TestItem.CreatedAt = DateTime.Now.Date;
+
+            TestList.Add(TestItem);
+            AllCustomers.CustomerList = TestList;
+            Assert.AreEqual(AllCustomers.Count, TestList.Count);
+
+        }
+    }
+}
